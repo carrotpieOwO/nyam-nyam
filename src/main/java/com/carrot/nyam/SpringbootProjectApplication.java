e83c2dd22880ef357d2828ab555dabbacd06a73a
carrotpieOwO <xxhayoxx@naver.com>
2020-04-24 16:58:34 +0900

    aws배포

--- src/main/java/com/carrot/nyam/SpringbootProjectApplication.java
+++ src/main/java/com/carrot/nyam/SpringbootProjectApplication.java
@@ -2,11 +2,17 @@ package com.carrot.nyam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
-public class SpringbootProjectApplication {
+public class SpringbootProjectApplication extends SpringBootServletInitializer{
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(SpringbootProjectApplication.class);
+	    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootProjectApplication.class, args);
--- src/main/java/com/carrot/nyam/config/WebConfig.java
+++ src/main/java/com/carrot/nyam/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.carrot.nyam.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,15 +8,15 @@ import org.springframework.web.servlet.resource.PathResourceResolver;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-
-	@Value("${file.path}")
-	private String fileRealPath;
-	
+//
+//	@Value("${file.path}")
+//	private String fileRealPath;
+//	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		WebMvcConfigurer.super.addResourceHandlers(registry);
 		
-		registry.addResourceHandler("/media/**").addResourceLocations("file:///" + fileRealPath).setCachePeriod(3600)
+		registry.addResourceHandler("/media/**").addResourceLocations("file:/var/media/").setCachePeriod(3600)
 		.resourceChain(true).addResolver(new PathResourceResolver());
 
 	}
